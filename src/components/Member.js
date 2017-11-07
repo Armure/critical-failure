4fdf390d6388912564bbb9d531582f61bc2de7ff
Jordan Munroe <munroejr@mcmaster.ca>
2017-11-06 19:42:36 -0500

    add cname

--- src/components/Member.js
+++ src/components/Member.js
@@ -3,8 +3,13 @@ import './Member.css'
 
 export default class Member extends React.Component {
   render () {
+    const {name, role, rank} = this.props
+    const image = 'https://render-us.worldofwarcraft.com/character/frostmane/240/42610672-main.jpg'
     return (
-      <div className='Member'>
+      <div className='Member' style={{backgroundImage: `url(${image})`}}>
+        <div className='Member-name'>{name}</div>
+        <div className='Member-name'>{role}</div>
+        <div className='Member-name'>{rank}</div>
       </div>
     )
   }
--- src/pages/Members.js
+++ src/pages/Members.js
@@ -2,14 +2,22 @@ import React from 'react'
 import Member from '../components/Member'
 import './Members.css'
 
+const data = [
+  {name: 'Atroce', role: 'dps', traitorRank: 1},
+  {name: 'Boltin', role: 'dps', traitorRank: 1},
+  {name: 'Spookieboogie', role: 'dps', traitorRank: 1},
+  {name: 'Krompobulous', role: 'tank', traitorRank: 3}
+]
+
 class Members extends React.Component {
+  getMembers () {
+    return data.map(m => <Member key={m.name} name={m.name} role={m.role} rank={m.traitorRank} />)
+  }
+
   render() {
     return (
       <div className='Members'>
-        <Member />
-        <Member />
-        <Member />
-        <Member />
+        {this.getMembers()}
       </div>
     )
   }
