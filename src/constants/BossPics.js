d3eb49ec5731333d9037028b474be16ec8009ea7
Jordan Munroe <jmunroe@spinvfx.com>
2017-12-04 11:38:24 -0500

    added antorus bosses

--- src/constants/BossPics.js
+++ src/constants/BossPics.js
@@ -1,21 +1,44 @@
-import goroth from '../assets/bosses/Goroth.jpg'
-import demonic from '../assets/bosses/Demonic.jpg'
-import host from '../assets/bosses/Host.jpg'
-import harjatan from '../assets/bosses/Harjatan.jpg'
-import sisters from '../assets/bosses/Sisters.jpg'
-import mistress from '../assets/bosses/Mistress.jpg'
-import maiden from '../assets/bosses/Maiden.jpg'
-import avatar from '../assets/bosses/Avatar.jpg'
-import kiljaeden from '../assets/bosses/Kiljaeden.jpg'
+import goroth from '../assets/bosses/tomb/Goroth.jpg'
+import demonic from '../assets/bosses/tomb/Demonic.jpg'
+import host from '../assets/bosses/tomb/Host.jpg'
+import harjatan from '../assets/bosses/tomb/Harjatan.jpg'
+import sisters from '../assets/bosses/tomb/Sisters.jpg'
+import mistress from '../assets/bosses/tomb/Mistress.jpg'
+import maiden from '../assets/bosses/tomb/Maiden.jpg'
+import avatar from '../assets/bosses/tomb/Avatar.jpg'
+import kiljaeden from '../assets/bosses/tomb/Kiljaeden.jpg'
+
+import garothi from '../assets/bosses/antorus/garothi.jpg'
+import felhounds from '../assets/bosses/antorus/felhounds.jpg'
+import highcommand from '../assets/bosses/antorus/highcommand.jpg'
+import hasabel from '../assets/bosses/antorus/hasabel.jpg'
+import eonar from '../assets/bosses/antorus/eonar.jpg'
+import imonar from '../assets/bosses/antorus/imonar.jpg'
+import kingaroth from '../assets/bosses/antorus/kingaroth.jpg'
+import varimathras from '../assets/bosses/antorus/varimathras.jpg'
+import coven from '../assets/bosses/antorus/coven.jpg'
+import aggramar from '../assets/bosses/antorus/aggramar.jpg'
+import argus from '../assets/bosses/antorus/argus.jpg'
 
 export default {
-  'Goroth': goroth,
-  'Demonic Inquisition': demonic,
-  'Harjatan': harjatan,
-  'Sisters of the Moon': sisters,
+  ['Goroth']: goroth,
+  ['Demonic Inquisition']: demonic,
+  ['Harjatan']: harjatan,
+  ['Sisters of the Moon']: sisters,
   [`Mistress Sassz'ine`]: mistress,
-  'The Desolate Host': host,
-  'Maiden of Vigilance': maiden,
-  'Fallen Avatar': avatar,
-  [`Kil'jaeden`]: kiljaeden
+  ['The Desolate Host']: host,
+  ['Maiden of Vigilance']: maiden,
+  ['Fallen Avatar']: avatar,
+  [`Kil'jaeden`]: kiljaeden,
+  [`Garothi Worldbreaker`]: garothi,
+  ['Felhounds of Sargeras']: felhounds,
+  ['Antoran High Command']: highcommand,
+  ['Portal Keeper Hasabel']: hasabel,
+  ['Eonar the Life-Binder']: eonar,
+  ['Imonar the Soulhunter']: imonar,
+  [`Kin'garoth`]: kingaroth,
+  ['Varimathras']: varimathras,
+  ['The Coven of Shivarra']: coven,
+  ['Aggramar']: aggramar,
+  ['Argus the Unmaker']: argus
 }
