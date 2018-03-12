7b0af9351890e03d23df94aabe440a84e6bc563f
Jordan Munroe <munroejr@mcmaster.ca>
2018-03-11 23:52:22 -0400

    added news

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -5,13 +5,12 @@ import './Members.css'
 const list = [
   {name: 'Atroce', role: 'dps', traitorRank: 1},
   {name: 'Boltin', role: 'dps', traitorRank: 1},
-  {name: 'Blackfoxrun', role: 'dps', traitorRank: 2},
+  {name: 'Blackfoxrun', role: 'dps', traitorRank: 3},
   {name: 'Fuusan', role: 'dps', traitorRank: 1},
-  {name: 'Krazeturtle', role: 'dps', traitorRank: 5},
-  {name: 'Krombôpulos', role: 'tank', traitorRank: 3},
+  {name: 'Krombôpulos', role: 'tank', traitorRank: 2},
   {name: 'Nemure', role: 'healer', traitorRank: 1},
   {name: 'Papie', role: 'healer', traitorRank: 1},
-  {name: 'Rosty', role: 'dps', traitorRank: 1},
+  {name: 'Rosty', role: 'dps', traitorRank: 1, realm: 'Frostmane'},
   {name: 'Spookyboogie', role: 'dps', traitorRank: 1},
   {name: 'Virb', role: 'dps', traitorRank: 1},
   {name: 'Wesleh', role: 'tank', traitorRank: 2},
@@ -22,10 +21,14 @@ class Members extends React.Component {
   getMembers () {
     const {data} = this.props
     if (!data) return null
-    return list.map(m => {
-      const character = data.filter(c => c.name === m.name)[0]
-      return <Member key={m.name} character={character} name={m.name} role={m.role} rank={m.traitorRank} />
-    })
+    return list.reduce((acc, m) => {
+      const character = data.filter(c => {
+        if (!m.realm) return c.name === m.name
+        return c.name === m.name && c.realm === m.realm
+      })[0]
+      if (character) acc.push(<Member key={m.name} character={character} name={m.name} role={m.role} rank={m.traitorRank} />)
+      return acc
+    }, [])
   }
 
   render() {
